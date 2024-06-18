cb06b685f32a7386a2440e92deddd1d65878e729
sorki <srk@48.io>
2024-06-18 18:43:29 +0200

    native: read/write aliases, complex types

--- src/Cidl/Backend/Haskell/Dict.hs
+++ src/Cidl/Backend/Haskell/Dict.hs
@@ -11,6 +11,7 @@ import Ivory.Artifact
 import Text.PrettyPrint.Mainland
 
 import qualified Cidl.Dict
+import qualified Cidl.Types.Base
 
 interfaceModule
   :: [String]
@@ -23,6 +24,8 @@ interfaceModule modulepath dict =
   stack $
     [ text "{-# LANGUAGE DeriveDataTypeable #-}"
     , text "{-# LANGUAGE DeriveGeneric #-}"
+    , text "{-# LANGUAGE FlexibleContexts #-}"
+    , text "{-# LANGUAGE RecordWildCards #-}"
     , text "{-# OPTIONS_GHC -fno-warn-unused-imports #-}"
     , empty
     , text "module"
@@ -31,6 +34,8 @@ interfaceModule modulepath dict =
     , empty
     , stack
         typeImports
+    , text "import Network.CANOpen.Class (MonadNode)"
+    , text "import Network.CANOpen.SDOClient (sdoRead, sdoWrite)"
     , text "import Network.CANOpen.Types"
     , empty
     , dictVarsAndFunctions dict
@@ -64,48 +69,219 @@ dictVarsAndFunctions
   -> Doc
 dictVarsAndFunctions dict =
   stack
-    [ variable entry
+    [     variables entry
+      </> complexAliases entry
     | entry <- Cidl.Dict.allEntries dict ]
 
 variable
   :: Entry
+  -> Maybe Int -- ^ Sub-index
   -> Doc
-variable Entry{..} =
+variable e@Entry{..} mSub =
   let
     camelName = Cidl.Utils.snakeToCamel entryName
     capName = Cidl.Utils.firstCap camelName
   in
-  case entryTyp of
-    PrimType _primType ->
-      stack
-      [     text camelName
-        <+> text ":: Variable"
-        <+> text (typeHaskellType entryTyp)
-      ,     text camelName
-        <+> equals
-        <+> text "Variable"
-      , indent 2
-          $ encloseStack
-              lbrace
-              rbrace
-              comma
-              [     text "variableName"
-                <+> equals
-                <+> dquotes (text capName)
-              ,     text "variableMux"
-                <+> equals
-                <+> text "Mux"
-                <+> text (Cidl.Utils.fmtHex entryIndex)
-                <+> text "0"
-              ,     text "variablePerm"
-                <+> equals
-                <+> buildPerm entryAccess
-              ]
-      ]
+    stack
+    [     text camelName
+      <+> text ":: Variable"
+      <+> text (typeHaskellType entryTyp)
+    ,     text camelName
+      <+> equals
+      <+> text "Variable"
+    , indent 2
+        $ encloseStack
+            lbrace
+            rbrace
+            comma
+            [     text "variableName"
+              <+> equals
+              <+> dquotes (text capName)
+            ,     text "variableMux"
+              <+> equals
+              <+> text "Mux"
+              <+> text (Cidl.Utils.fmtHex entryIndex)
+              <+> text (maybe "0" show mSub)
+            ,     text "variablePerm"
+              <+> equals
+              <+> buildPerm entryAccess
+            ]
+    , alias e
+    ]
 
+-- | Calls variable multiple times for complex types
+variables
+  :: Entry
+  -> Doc
+variables e =
+  case entryTyp e of
+    PrimType _primType ->
+      variable e Nothing
+    RecordType _recordName subEntries ->
+          variable
+            (e
+              { entryTyp = Cidl.Types.Base.uint8
+              , entryName = entryName e <> "_count"
+              }
+            )
+            Nothing
+      </> stack
+            [ variable
+                (sub
+                  { entryName = entryName e <> "_" <> entryName sub })
+                  (Just subIdx)
+            | (sub, subIdx) <- zip subEntries [(1 :: Int) .. ]
+            ]
+    ArrayType _arrName arrLen arrTyp ->
+          variable
+            (e
+              { entryTyp = Cidl.Types.Base.uint8
+              , entryName = entryName e <> "_count"
+              }
+            )
+            Nothing
+      </> stack
+            [ variable
+                (e
+                  { entryTyp = arrTyp
+                  , entryName = entryName e <> show subIdx
+                  }
+                )
+                (Just subIdx)
+            | subIdx <- [(1 :: Int) .. arrLen ]
+            ]
+
+    -- VarArrayType hodl
     _ -> empty
 
 buildPerm :: Perm -> Doc
 buildPerm p =
      text "Permission_"
   <> text (show p)
+
+-- | sdoRead/write alias
+alias
+  :: Entry
+  -> Doc
+alias Entry{..} =
+  let
+    camelName = Cidl.Utils.snakeToCamel entryName
+    capName = Cidl.Utils.firstCap camelName
+  in
+    stack
+      [ if (entryAccess `elem` [Read, ReadWrite])
+        then stack
+          [     text "read"
+            <>  text capName
+            <+> text "::"
+            <+> text "MonadNode m"
+            <+> text "=>"
+            <+> text "m"
+            <+> text (typeHaskellType entryTyp)
+          ,     text "read"
+            <>  text capName
+            <+> text "="
+            <+> text "sdoRead"
+            <+> text camelName
+          ]
+        else empty
+      , if (entryAccess `elem` [Write, ReadWrite])
+        then stack
+          [     text "write"
+            <>  text capName
+            <+> text "::"
+            <+> text "MonadNode m"
+            <+> text "=>"
+            <+> text (typeHaskellType entryTyp)
+            <+> text "->"
+            <+> text "m ()"
+          ,     text "write"
+            <>  text capName
+            <+> text "="
+            <+> text "sdoWrite"
+            <+> text camelName
+          ]
+
+        else empty
+      ]
+
+-- | sdoRead/Write aliases for complex types
+-- Generated iff all fields are readable/writeable
+complexAliases
+  :: Entry
+  -> Doc
+complexAliases e =
+  let
+    camelName = Cidl.Utils.snakeToCamel (entryName e)
+    capName = Cidl.Utils.firstCap camelName
+  in
+  case entryTyp e of
+    PrimType _primType -> empty
+    RecordType _recordName subEntries ->
+      stack
+        [ if all (\s -> entryAccess s `elem` [Read, ReadWrite]) subEntries
+          then stack
+            [     text "read"
+              <>  text capName
+              <+> text "::"
+              <+> text "MonadNode m"
+              <+> text "=>"
+              <+> text "m"
+              <+> text (typeHaskellType (entryTyp e))
+            ,     text "read"
+              <>  text capName
+              <+> text "= do"
+            , indent 2
+                $ stack
+                  [     text (Cidl.Utils.snakeToCamel $ entryName s)
+                    <+> text "<-"
+                    <>  text
+                          (Cidl.Utils.snakeToCamel
+                            $  "read_"
+                            <> entryName e
+                            <> "_"
+                            <> entryName s
+                          )
+                  | s <- subEntries
+                  , entryAccess s `elem` [Read, ReadWrite]
+                  ]
+            , indent 2
+                $   text "pure"
+                <+> text (typeHaskellType (entryTyp e))
+                <>  text "{..}"
+            ]
+          else empty
+        , if all (\s -> entryAccess s `elem` [Write, ReadWrite]) subEntries
+          then stack
+            [     text "write"
+              <>  text capName
+              <+> text "::"
+              <+> text "MonadNode m"
+              <+> text "=>"
+              <+> text (typeHaskellType (entryTyp e))
+              <+> text "->"
+              <+> text "m ()"
+            ,     text "write"
+              <>  text capName
+              <+> text (typeHaskellType (entryTyp e))
+              <>  text "{..}"
+              <+> text "= do"
+            , indent 2
+                $ stack
+                  [     text
+                          (Cidl.Utils.snakeToCamel
+                            $  "write_"
+                            <> entryName e
+                            <> "_"
+                            <> entryName s
+                          )
+                    <+> text (Cidl.Utils.snakeToCamel $ entryName s)
+                  | s <- subEntries
+                  , entryAccess s `elem` [Read, ReadWrite]
+                  ]
+            ]
+          else empty
+        ]
+
+    -- VarArrayType hodl
+    _ -> empty
